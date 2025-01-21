bde677ce52dc6f06928d67772a4a63e65bb846d9
Thanh <kathyngoan@gmail.com>
2025-01-21 18:14:48 +0700

    Fix link in vercel

--- src/components/FancyFormComponent.jsx
+++ src/components/FancyFormComponent.jsx
@@ -185,9 +185,9 @@ const FancyFormComponent = () => {
         <button
           type="submit"
           className="w-full py-2 px-4 text-white font-medium rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 
-  hover:from-black hover:via-black hover:to-black 
-  focus:border-none hover:border-none
-  transition duration-200 font-bold"
+            hover:from-black hover:via-black hover:to-black 
+            focus:border-none hover:border-none
+            transition duration-200 font-bold"
         >
           CONFIRM SWAP
         </button>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   const boxes = [
     {
@@ -22,28 +25,25 @@ const HomePage = () => {
 
   return (
     <div className="text-white py-10">
-      {/* Boxes */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:px-16 px-5 items-center">
         <div>
-          <h1 className="text-4xl font-bold mb-10 text-orange-300" data-aos="fade-in">Welcome to My Test</h1>
-          <div className="uppercase" data-aos="fade-in" ><span className="inline-flex w-[100px]"data-aos-delay="300">Full Name : </span>Nguyen Thi Dan Thanh</div>
-          <div data-aos="fade-in" data-aos-delay="600" ><span className="inline-flex w-[100px]">Position : </span>Front End Developer</div>
-          <div data-aos="fade-in" data-aos-delay="600" ><span className="inline-flex w-[100px]">Email : </span>kathyngoan@gmail.com</div>
+          <h1 className="text-4xl font-bold mb-10 text-orange-300">Welcome to My Test</h1>
+          <div className="uppercase"><span className="inline-flex w-[100px]">Full Name : </span>Nguyen Thi Dan Thanh</div>
+          <div><span className="inline-flex w-[100px]">Position : </span>Front End Developer</div>
+          <div><span className="inline-flex w-[100px]">Email : </span>kathyngoan@gmail.com</div>
         </div>
         <div className="grid grid-cols-1 gap-8">
-          {boxes.map((box, index) => (
-            <a
+          {boxes.map((box) => (
+            <Link
               key={box.id}
-              href={box.link}
+              to={box.link}
               className={`box p-6 rounded-lg shadow-md transform hover:scale-105 transition-transform duration-300`}
-              data-aos="fade-in"
-              data-aos-delay={index * 300}
             >
               <h2 className="oxanium-font text-xl uppercase font-bold text-yellow-50 mb-2">
                 {box.title}
               </h2>
               <p className="text-gray-100">{box.description}</p>
-            </a>
+            </Link>
           ))}
         </div>
       </div>
